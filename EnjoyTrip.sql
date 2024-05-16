a09f30afe9bcd93786e6d0fea137d9c1d6b8d2a6
방태연 <imbhang3@naver.com>
2024-05-16 17:50:25 +0900

    modify sql ddl

--- EnjoyTrip.sql
+++ EnjoyTrip.sql
@@ -1,3 +1,5 @@
+use ssafytrip;
+
 CREATE TABLE `members` (
   `user_id` varchar(16) NOT NULL,
   `user_name` varchar(20) NOT NULL,
@@ -6,6 +8,10 @@ CREATE TABLE `members` (
   `join_at` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
 );
+
+alter table members
+add column token varchar(1000)
+null default null after join_at;
 
 CREATE TABLE `content_type`(
     `content_type_id` INT NOT NULL,
@@ -51,3 +57,50 @@ CREATE TABLE hotplace (
     FOREIGN KEY (`user_id`) REFERENCES `members`(`user_id`)
 );
 
+CREATE TABLE IF NOT EXISTS comment (
+    `comment_no` INT AUTO_INCREMENT PRIMARY KEY,
+    `board_no` INT,
+    `user_id` varchar(16),
+    `content` varchar(1000),
+    `create_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (`board_no`) REFERENCES board(`board_no`) on delete cascade,
+    FOREIGN KEY (`user_id`) REFERENCES members(`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS plan_seq (
+	`plan_no` INT,
+    `seq_no` INT,
+    `content_id` INT,
+    PRIMARY KEY (`plan_no` , `seq_no`),
+    FOREIGN KEY (`content_id`) REFERENCES attraction_info(`content_id`) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS plan_user (
+	`plan_no` INT,
+    `user_id` varchar(16),
+    foreign key (`plan_no`) references plan_seq(`plan_no`) on delete cascade,
+    foreign key (`user_id`) references members(`user_id`) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS hotplace_info (
+    hotplace_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(16),
+    content_type_id INT,
+    title VARCHAR(100),
+    addr1 VARCHAR(100),
+    addr2 VARCHAR(100),
+    tel VARCHAR(50),
+    image_src VARCHAR(200),
+    sido_code int,
+    gugun_code int,
+    latitude decimal(20, 17),
+    longitude decimal(20, 17),
+    FOREIGN KEY (user_id) REFERENCES members(user_id),
+    FOREIGN KEY (content_type_id) REFERENCES content_type(content_type_id)
+);
+
+CREATE TABLE IF NOT EXISTS hotplace_description (
+    hotplace_id INT,
+    overview VARCHAR(10000),
+    FOREIGN KEY (hotplace_id) REFERENCES hotplace_info(hotplace_id)
+);
